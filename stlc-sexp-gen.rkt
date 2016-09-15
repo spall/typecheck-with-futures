421439c271225265676f8ca0c45aaab6bea2bf48
Sarah Spall <sspall10@gmail.com>
2016-09-15 11:30:31 -0400

    now generates well formed sexp

--- stlc-sexp-gen.rkt
+++ stlc-sexp-gen.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-(require "s-exp-stlc.rkt")
+(require "s-exp-stlc.rkt"
+         racket/random)
+(provide gen-sexp)
 
 #| http://www.cs.cornell.edu/courses/cs6110/2013sp/lectures/lec25-sp13.pdf
 
@@ -21,7 +23,7 @@
       [(= rand 1)
        'bool]
       [else
-       `(,(gen-type) ,(gen-type))])))
+       `(,(gen-type) -> ,(gen-type))])))
 
 (define (gen-prim-value)
   (let ([rand (random 3)])
@@ -44,18 +46,77 @@
      (let ([rand (random 2)])
        (cond
          [(= rand 0) ;; app
-          `(,(gen-lambda (ceiling (/ size 2)) ls) ,(gen-term (ceiling (/ size 2)) ls))]
+          (gen-application (- size 1) ls)]
          [else ;; lambda
           (gen-lambda (- size 1) ls)]))]))
 
-;; could generate expressions that return lambdas.  meaning application
+
+;; generates nested lambdas.
 (define (gen-lambda size ls)
+  (if (= 1 size)
+      (let ([arg (gensym)])
+        `(lambda (,arg : ,(gen-type))
+           ,(gen-term 0 (cons arg ls))))
+        
+      (let ([arg (gensym)])
+        `(lambda (,arg : ,(gen-type))
+           ,(gen-lambda (sub1 size) (cons arg ls))))))
+
+(define (gen-lambda-w-body body)
   (let ([arg (gensym)])
     `(lambda (,arg : ,(gen-type))
-       ,(gen-term (- size 1) (cons arg ls)))))
+       ,body)))
+
+;; takes a lambda, generates necessary parameters
+(define (gen-parameters lam)
+  (match lam
+    [`(lambda (,(? symbol? arg) : ,t1) ,body)
+     (if (is-lambda? body)
+         (cons (gen-term-w-type t1)
+               (gen-parameters body))
+         (cons (gen-term-w-type t1)
+               '()))]
+    [else
+     (error 'gen-application "not a lambda ~v~n" lam)]))
+
+;; constructs an application from lambda and params
+(define (gen-application expr params)
+  (cond
+    [(empty? params)
+     expr]
+    [else
+     (gen-application `(,expr ,(car params))
+                      (cdr params))]))
+  
+;; expr has type lambda?
+(define (is-lambda? expr)
+  (match (typecheck-expr expr)
+    [`(,t1 -> ,t2)
+     #t]
+    [_
+     #f]))
+    
+;; generates term with type t
+(define (gen-term-w-type t)
+  (match t
+    ['int
+     (random 100)]
+    ['bool
+     (random-ref (list 'true 'false))]
+    ['ntype
+     'null]
+    [`(,(? type? t1) -> ,(? type? t2))
+     `(lambda (,(gensym) : ,t1) ,(gen-term-w-type t2))]
+    [else
+     (error 'gen-term-w-type "not a valid type ~v" t)]))
 
 
 (define (gen-sexp size)
   (gen-term size '()))
+
+(define (gen-well-formed-sexp size)
+  (let ([lam (gen-lambda size '())])
+    (gen-application lam (gen-parameters lam))))
+                   
 
   
