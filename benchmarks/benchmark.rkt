5f860bb688b84b81f77d4bc7d1106793749951c8
Sarah Spall <sspall10@gmail.com>
2016-10-17 19:01:42 -0400

    small change

--- benchmarks/benchmark.rkt
+++ benchmarks/benchmark.rkt
@@ -1,22 +1,23 @@
 #lang racket
 
-(require "../s-exp-stlc.rkt")
+(require "../s-exp-stlc.rkt"
+         future-visualizer)
 (provide time-typechecker)
 
 (define (time-typechecker exprs)
   (define ITER 10)
-  
+  (define list-exprs (list exprs))
   (let*-values ([(ssum nsum bsum) (for/fold ([ssum_ 0]
-                                             [nsum_ 0]
-                                             [bsum_ 0])
-                                            ([_ (in-range ITER)])
-                                    (let-values ([(a b srt c) (time-apply typecheck-sequential (list exprs))]
-                                                 [(f e nrt d) (begin (collect-garbage) (collect-garbage) (collect-garbage)
-                                                                (time-apply naive-typecheck-parallel (list exprs)))]
-                                                 [(g h brt i) (begin (collect-garbage) (collect-garbage) (collect-garbage)
-                                                                     (time-apply better-typecheck-parallel (list exprs)))])
-                                      (collect-garbage)(collect-garbage)(collect-garbage)
-                                      (values (+ ssum_ srt) (+ nsum_ nrt) (+ bsum_ brt))))]
+                                                  [nsum_ 0]
+                                                  [bsum_ 0])
+                                                 ([_ (in-range ITER)])
+                                         (let-values ([(a b srt c) (time-apply typecheck-sequential list-exprs)]
+                                                      [(f e nrt d) (begin (collect-garbage) (collect-garbage) (collect-garbage)
+                                                                          (time-apply naive-typecheck-parallel list-exprs))]
+                                                      [(g h brt i) (begin (collect-garbage) (collect-garbage) (collect-garbage)
+                                                                          (time-apply better-typecheck-parallel list-exprs))])
+                                           (collect-garbage)(collect-garbage)(collect-garbage)
+                                           (values (+ ssum_ srt) (+ nsum_ nrt) (+ bsum_ brt))))]
                 [(savg) (/ ssum ITER)]
                 [(navg) (/ nsum ITER)]
                 [(bavg) (/ bsum ITER)])
